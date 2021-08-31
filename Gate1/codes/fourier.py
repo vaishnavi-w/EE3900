c0c74d03beb8dfe7b50f2882e0867d2d4a2d71ea
Vaishnavi <80573434+vaishnavi-w@users.noreply.github.com>
2021-08-31 07:59:39 +0100

    Update fourier.py

--- Gate1/codes/fourier.py
+++ Gate1/codes/fourier.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.fftpack
-N = 1000
+N = 500
 
 # data
-x = np.linspace(-5*np.pi,5*np.pi, N)
+x = np.linspace(-3,3, N)
 
 y = np.zeros(N)
 for i in range(N):
-  if x[i] >= -4*np.pi and x[i] <= 4*np.pi:
+  if x[i] >= -2 and x[i] <= 2:
     y[i] = 0.25
   
-fourier = [np.mean(y * np.exp( 1j * f * x)) for f in x]
+fourier = [np.mean(y * np.exp( 1j * 2 * np.pi * f * x)) for f in x]
 
 # figure
 plt.plot(x,y)
-plt.xlabel('t')
-plt.ylabel('f(t)')
-plt.title('Signal in time domain')
+plt.xlabel('f')
+plt.ylabel('X(f)')
+plt.title('Frequency domain')
 plt.show()
 plt.plot(x,np.real(fourier))
-plt.xlabel('\u03C9')
-plt.ylabel('Re(F(\u03C9))')
-plt.title('Signal in frequency domain')
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.title('Time domain')
 plt.show()
